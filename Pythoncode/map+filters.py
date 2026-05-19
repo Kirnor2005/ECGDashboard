5304c9be1d678d2923a174aab3d1b1db602bb846
Kirnor2005 <72357472+Kirnor2005@users.noreply.github.com>
2026-05-19 11:35:43 +0200

    Refactor filter selection and layout in Streamlit app

--- Pythoncode/map+filters.py
+++ Pythoncode/map+filters.py
@@ -1,31 +1,70 @@
 import streamlit as st
 
-col2, col1 = st.columns([1, 4])
+st.set_page_config(layout="wide")
 
-with col1:
+col2, col1 = st.columns([4, 1])
+
+with col2:
     st.image('amsterdam-map.jpg')
     
-with col2: 
+with col1: 
     st.expander('Settings', icon="⚙️")
     
-    gekozen_filters = st.multiselect(
-        "Filters",
-        ["Geslacht", "Leeftijd", "Wijk", "Waarde"]
-    )
+    import streamlit as st
 
-    filter_opties = {
+    FILTERS = {
         "Geslacht": ["Man", "Vrouw", "Anders"],
         "Leeftijd": ["40-50", "50-60", "60-70", "70+"],
-        "Wijk": ["Sloterdijk Nieuw-West", "Geuzenveld, Slotermeer", "Osdorp", "De Aker, Sloten, Nieuw-Sloten", "Slotervaart", "Westerpark", "Bos en Lommer", "Oud-West, De Baarsjes", "Oud-Zuid", "Buitenveldert", "Amsterdamse Bos", "Centrum-West", "Centrum-Oost", "Oud-Oost", "De Pijp, Rivierenbuurt", "Watergraafsmeer", "Indische Buurt, Oostelijk Havengebied", "Oud-Noord", "Noord-West", "Noord-Oost", "IJburg, Zeeburgereiland", "Bijlmer-Centrum", "Bijlmer-West", "Bijlmer-Oost", "Gaasperdam", "Weesp, Driemond"],
-        "Waarde": ["Gem HR totaal", "HRV", "Bloeddruk (Bovendruk)", "Cholesterol", "Non-HDL", "Bloedsuiker", "BMI"]
+        "Wijk": [
+            "Sloterdijk Nieuw-West",
+            "Geuzenveld, Slotermeer",
+            "Osdorp",
+            "De Aker, Sloten, Nieuw-Sloten",
+            "Slotervaart",
+            "Westerpark",
+            "Bos en Lommer",
+            "Oud-West, De Baarsjes",
+            "Oud-Zuid",
+            "Buitenveldert",
+            "Amsterdamse Bos",
+            "Centrum-West",
+            "Centrum-Oost",
+            "Oud-Oost",
+            "De Pijp, Rivierenbuurt",
+            "Watergraafsmeer",
+            "Indische Buurt, Oostelijk Havengebied",
+            "Oud-Noord",
+            "Noord-West",
+            "Noord-Oost",
+            "IJburg, Zeeburgereiland",
+            "Bijlmer-Centrum",
+            "Bijlmer-West",
+            "Bijlmer-Oost",
+            "Gaasperdam",
+            "Weesp, Driemond",
+        ],
+        "Waarde": [
+            "Gem HR totaal",
+            "HRV",
+            "Bloeddruk (Bovendruk)",
+            "Cholesterol",
+            "Non-HDL",
+            "Bloedsuiker",
+            "BMI",
+        ],
     }
 
-    #geselecteerde_waardes = {}
+    gekozen_filters = st.multiselect(
+        "Filters",
+        options=list(FILTERS.keys())
+    )
 
-    #for filter_naam in gekozen_filters:
-     #   geselecteerde_waardes[filter_naam] = st.multiselect(
-      #      f"Kies {filter_naam}",
-       #     filter_opties[filter_naam]
-        #)
-
-    #st.write(geselecteerde_waardes)
+    geselecteerde_waardes = {
+        naam: st.multiselect(
+            f"Kies {naam}",
+            options=opties,
+            key=f"filter_{naam}"
+        )
+        for naam, opties in FILTERS.items()
+        if naam in gekozen_filters
+    }
