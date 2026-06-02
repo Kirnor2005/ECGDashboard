39e888d2e6e5f65aac6606ac7e0751aa5b149f53
martinsalicia23-spec <martinsalicia23@gmail.com>
2026-06-02 14:15:08 +0200

    Update app.py
    lat

--- Pythoncode/app.py
+++ Pythoncode/app.py
@@ -358,7 +358,7 @@ def main_app():
                     hover_name = 'Gebied',
                     mapbox_style="carto-positron",
                     zoom=9.5,
-                    center={"lat": 51.37, "lon": 4.69},
+                    center={"lat": 51.35, "lon": 4.89},
                     opacity=0.4,
                 )
 
