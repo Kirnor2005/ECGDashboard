6ad9a6ddcee6e0455120764d75b13866421f0c60
Kirnor2005 <72357472+Kirnor2005@users.noreply.github.com>
2026-06-06 14:17:25 +0200

    Enhance session state management and data loading
    Refactor session state initialization and update data loading functions.

--- Pythoncode/app.py
+++ Pythoncode/app.py
@@ -6,7 +6,76 @@ from pathlib import Path
 import geopandas as gpd
 import base64
 
+
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+if 'pagina' not in st.session_state:
+        st.session_state.pagina = 'kaart'
+    
+if 'wijken_graf' not in st.session_state:
+        st.session_state.wijken_graf = []
+    
+if 'wijken_tab' not in st.session_state:
+        st.session_state.wijken_tab = []
+    
+if 'variabelen' not in st.session_state:
+        st.session_state.variabelen = []
+
+if 'leeftijd' not in st.session_state:
+    st.session_state.leeftijd = ['40-50', '50-60', '60-70'] # Verander naar [] als je wilt dat standaard niets geselecteerd is.
+    
+if 'geslacht' not in st.session_state:
+    st.session_state.geslacht = ['man', 'vrouw', 'anders'] # idem
+    
+if 'taal' not in st.session_state:
+        st.session_state.taal = 'English'
+    
+if 'mvc' not in st.session_state:
+        st.session_state.mvc = []
+
+if 'fontsize_scale' not in st.session_state:
+    st.session_state.fontsize_scale = 1.0
+
+#page layout
+
 st.set_page_config(layout="wide")
+if st.session_state.taal == 'العربية':
+    direction = 'rtl'
+    align = 'right'
+else:
+    direction = 'ltr'
+    align = 'left'
+    
+st.markdown(f"""
+<style>
+html, body, .stApp {{
+    direction: {direction};
+    text-align: {align};
+}}
+
+p, div, span, label,
+h1, h2, h3, h4, h5, h6 {{
+    direction: {direction};
+    text-align: {align};
+}}
+
+input, textarea {{
+    direction: {direction};
+    text-align: {align};
+}}
+
+
+/* EXCLUDE slider */
+[data-testid="stSlider"] {{
+    direction: ltr !important;
+}}
+[data-testid="stSlider"] * {{
+    direction: ltr !important;
+}}
+
+</style>
+""", unsafe_allow_html=True)
 
 st.markdown("""
 <style>
@@ -88,18 +157,29 @@ div[data-testid="stAlert"] {
 </style>
 """, unsafe_allow_html=True)
 
+
 BASE_DIR = Path(__file__).resolve().parent
 
 @st.cache_data
 def load_data():
-    return pd.read_csv(BASE_DIR.parent / "Excel bestanden" / "verwerkte_data.csv")
+    return pd.read_csv(BASE_DIR.parent / "Excel bestanden" / "verwerkte_data_alles.csv")
 
 @st.cache_data
 def load_talen():
     return pd.read_csv(BASE_DIR.parent / "Excel bestanden" / "taal.csv", sep=';')
 
+@st.cache_data
+def load_data_all():
+    return pd.read_csv(BASE_DIR.parent / "Excel bestanden" / "verwerkte_data_geslacht_leeftijd_final.csv", sep=';')
+
+
 df = load_data()
 talen = load_talen()
+df_alles = load_data_all()
+
+#df = pd.read_csv('verwerkte_data.csv')
+#talen = pd.read_csv("taal.csv", sep=';')
+#df_alles = pd.read_csv(r"C:\Users\Kirno\Streamlit\verwerkte_data_geslacht_leeftijd_final.csv", sep=';')
 
 talen.columns = talen.columns.str.strip()
 talen['key'] = talen['key'].str.strip()
@@ -144,32 +224,25 @@ mapping = {
     'ex bmi gem 2': 'c bmi2 gem'
 }
 
+talen_opties = [
+    'Nederlands',
+    'English',
+    'العربية',
+    'Français',
+    'Español',
+    'Deutsch',
+    'Português (Brasil)',
+    'Русский',
+    '中文',
+    '日本語',
+    '한국인',
+    'हिंदी',
+    'বাংলা', #bengali
+    'Bahasa Indonesia'
+]
+
 CORRECT_EMAIL = "jan_pieters@gmail.com"
 CORRECT_PASSWORD = "Hartstikke_gezond123"
-
-if "logged_in" not in st.session_state:
-    st.session_state.logged_in = False
-
-if 'pagina' not in st.session_state:
-        st.session_state.pagina = 'kaart'
-    
-if 'wijken_graf' not in st.session_state:
-        st.session_state.wijken_graf = []
-    
-if 'wijken_tab' not in st.session_state:
-        st.session_state.wijken_tab = []
-    
-if 'variabelen' not in st.session_state:
-        st.session_state.variabelen = []
-    
-if 'taal' not in st.session_state:
-        st.session_state.taal = 'English' #mag weer veranderd worden naar English maar is nu ff Deutsch om te demonstereren dat het ook voor het loginscherm werkt.
-    
-if 'mvc' not in st.session_state:
-        st.session_state.mvc = []
-
-if 'fontsize_scale' not in st.session_state:
-    st.session_state.fontsize_scale = 1.0
 
 #functie die ervoor zorgt dat het makkelijker is om tussen talen te switchen dan hoef je niet elke keer talen.loc['key', st.session_state.taal] te typen
 def t(key):
@@ -185,10 +258,49 @@ def centered_image(image_path, width=200):
     # -rik
     st.markdown(f'<div style="display: flex; justify-content: center;"><img src="data:image/png;base64,{encoded}" width="{width}"></div>',unsafe_allow_html=True)
 
+
 #"hartstikke-gezondweek.png"
 
 def login_screen():
-    centered_image(BASE_DIR / "hartstikke-gezondweek.png", width=200)
+    #centered_image(BASE_DIR / "hartstikke-gezondweek.png", width=200)
+    centered_image("hartstikke-gezondweek.png", width=200)
+
+    # beweegt mee met resize van tab
+    st.markdown(""" 
+    <style> 
+    /*alleen popover icon */
+    [data-testid="stPopover"] button p { 
+        font-size: 1.5em; 
+        line-height: 1; 
+    }
+
+    /*verwijder de pijl (expand_less) */
+    [data-testid="stPopover"] [data-testid="stIconMaterial"] {
+        display: none !important;
+    }
+
+    /*hoogte van de popover in relatie met de rest*/
+    [data-testid="stPopover"] {
+        position: fixed;
+        left: 30rem;
+        top: 10rem;
+        z-index: 9999;
+    }
+
+    /*zorgt ervoor dat er geen border is*/
+        [data-testid="stPopover"] button {
+        border: none;
+        box-shadow: none;
+        background: transparent;
+        padding: 0;
+    }
+    </style> 
+    """, unsafe_allow_html=True)
+            
+    with st.popover('⚙'):
+                selectionbox = st.selectbox(t('mp taal'),options=talen_opties,key='taal')
+                index=talen_opties.index(st.session_state.taal)
+                st.slider(label=t('mp lett'), min_value=0.2, max_value=3.5, value=st.session_state.fontsize_scale, step=0.1, key='fontsize_scale')
 
     st.markdown(
         "<div class='login-title-text'>Inloggen</div>",
@@ -225,85 +337,40 @@ def main_app():
     
     with col1:
 
-        st.markdown("<div style='height: 1rem'></div>", unsafe_allow_html=True)
-
-        st.markdown("<div style='height: 1rem'></div>", unsafe_allow_html=True)
-    
-        with st.expander(t('mp Opties'), expanded=False):
-            font = st.slider(
-                label=t('mp lett'), 
-                min_value=0.2, 
-                max_value=3.5, 
-                value=st.session_state.fontsize_scale, 
-                step=0.1, 
-                key='fontsize_scale')
-
-            scale = st.session_state.fontsize_scale
-
-            st.markdown(
-                f"""
-                <style>
-                html, body, [class*="css"] {{
-                font-size: {16 * scale}px;
-                }}
-                </style>
-                """,
-                unsafe_allow_html=True
-            )
-    
-            talen_opties = [
-                'Nederlands',
-                'English',
-                'العربية',
-                'Français',
-                'Español',
-                'Deutsch',
-                'Português',
-                'Русский',
-                '中文',
-                '日本語',
-                '한국인'
-            ]
-    
-            selectionbox = st.selectbox(
-                t('mp taal'),
-                options=talen_opties,
-                index=talen_opties.index(st.session_state.taal),
-                key='taal'
-            )
-            
-                         
+        st.markdown("<div style='height: 6rem'></div>", unsafe_allow_html=True)
+        
+                     
         with st.expander(t('mp filter'), expanded=False): # beide de grafiek en tabel kunnen in een st.container (?)
-    
+
             geselecteerde_wijken_grafiek = st.multiselect(
                 t('mp wijk g'),
                 options=df['Wijk'].unique(),
                 default=st.session_state.wijken_graf,
                 placeholder=t('menu co')
             )
-    
-    
+
+
             var_keys = pd.concat([talen.loc["ex sdnn_1":"ex HR Diff"],talen.loc["ex bd gem 1":"ex bmi gem 2"]]).index.tolist()
-    
+
             geselecteerde_variabelen = st.multiselect(t('mp kies v'), #return = keys van geselecteerde waarden, dus niet de waarden die de gebruiker ziet.
-                                                      options=var_keys, #lijst met opties
-                                                      default=st.session_state.variabelen, #als er niks gekozen is, is het leeg
-                                                      format_func=lambda x: talen.loc[x, st.session_state.taal], 
-                                                      placeholder=t('menu co')
-                                                     )
-    
-            
-            
-           
-    
+                                                  options=var_keys, #lijst met opties
+                                                  default=st.session_state.variabelen, #als er niks gekozen is, is het leeg
+                                                  format_func=lambda x: talen.loc[x, st.session_state.taal], 
+                                                  placeholder=t('menu co')
+                                                 )
+
+        
+        
+       
+
             # knop
             if st.button(t('mp toon g')):
-    
+
                 st.session_state.wijken_graf = geselecteerde_wijken_grafiek
                 st.session_state.variabelen = geselecteerde_variabelen
-    
+
                 st.session_state.pagina = 'grafiek'
-    
+
                 st.rerun()
     
             geselecteerde_wijken_tabel = st.multiselect(
@@ -312,30 +379,46 @@ def main_app():
                 default=st.session_state.wijken_tab,
                 placeholder=t('menu co')
             )
-    
-    
+
+
             # zorgt ervoor dat de keys die nu indexes zijn in een lijst worden gezet
             mvc_keys = talen.loc["ex mvc bd1":"ex mvc bmi2"].index.tolist()
-    
-    
+
+
             meest_voorkomende_categorieën = st.multiselect(t('mp mvc'), # return = keys van geselecteerde waarden, dus niet de waarden die de gebruiker ziet.
                                                            options=mvc_keys, #lijst met opties
                                                            default=st.session_state.mvc, #als er niks gekozen is, is het leeg
                                                            format_func=lambda x: talen.loc[x, st.session_state.taal], #
                                                            placeholder=t('menu co')
                                                           )
-            
+        
             # knop
             if st.button(t('mp toon t')):
-    
+
                 st.session_state.wijken_tab = geselecteerde_wijken_tabel
                 st.session_state.mvc = meest_voorkomende_categorieën
-    
+
                 st.session_state.pagina = 'tabel'
-    
+
                 st.rerun()
-    
-    
+
+
+            leeftijds_categorie = st.multiselect(t('mp leeftijd'), 
+                                                 options = ['40-50', '50-60', '60-70'],
+                                                 default = st.session_state.leeftijd,
+                                                 placeholder = t('mp leeftijd')
+                                                )
+            
+            geslacht_keys = talen.loc['man':'anders'].index.tolist()
+            geslacht_kiezen = st.multiselect(t('mp geslacht'),
+                                             options = geslacht_keys,
+                                             default = st.session_state.geslacht,
+                                             format_func =lambda x: talen.loc[x, st.session_state.taal],
+                                             placeholder = t('mp geslacht')
+                                            )
+                                                                     
+
+
     # rechter kolom
     
     
@@ -343,8 +426,68 @@ def main_app():
 
         # pagina 1 = kaart
         if st.session_state.pagina == 'kaart':
-        
-            st.title(t('mp tit kaart'))
+
+            header = st.container()
+
+            with header:
+                hcol1, hcol2 = st.columns([6, 1])
+                with hcol1:
+                    st.markdown(
+                    f"<h1 style='margin:0'>{t('mp tit kaart')}</h1>",
+                    unsafe_allow_html=True
+                    )
+                    
+                    #st.title(t('mp tit kaart'))
+
+                with hcol2:
+                    st.markdown(""" 
+                    <style> 
+                    /*alleen popover icon */
+                    [data-testid="stPopover"] button p { 
+                        font-size: 1.5em; 
+                        line-height: 1; 
+                    }
+
+                    /*verwijder de pijl (expand_less) */
+                    [data-testid="stPopover"] [data-testid="stIconMaterial"] {
+                        display: none !important;
+                    }
+
+                    /*hoogte van de popover in relatie met de rest*/
+                    [data-testid="stPopover"] {
+                        position: relative;
+                        top: 30px;
+                    }
+
+                    /*zorgt ervoor dat er geen border is*/
+                    [data-testid="stPopover"] button {
+                        border: none;
+                        box-shadow: none;
+                        background: transparent;
+                        padding: 0;
+                    }
+                    </style> 
+                    """, unsafe_allow_html=True)  
+                    
+                    with st.popover('⚙'):
+                        selectionbox = st.selectbox(t('mp taal'),options=talen_opties,key='taal')
+                        ##index=talen_opties.index(st.session_state.taal)
+                        st.slider(label=t('mp lett'), min_value=0.2, max_value=3.5, value=st.session_state.fontsize_scale, step=0.1, key='fontsize_scale')
+
+            scale = st.session_state.fontsize_scale
+
+            st.markdown(
+                f"""
+                <style>
+                html, body, [class*="css"] {{
+                    font-size: {16 * scale}px;
+                }}
+                </style>
+                """,
+                unsafe_allow_html=True
+            )
+            
+
         
             try:
                 buurten = gpd.read_file('https://maps.amsterdam.nl/open_geodata/geojson_lnglat.php?KAARTLAAG=INDELING_GEBIED&THEMA=gebiedsindeling')
@@ -362,12 +505,16 @@ def main_app():
                     opacity=0.45,
                 )
 
+                fig2.update_layout(
+                    margin=dict(l=0, r=0, t=0, b=0) #verwijderd marges (l=left, r=right, t=top, b=bottom), als je het weer naar het origineel wil hebben zonder dat de bovenkant weer ver van de titel af staat doe: t=0, b=140, toegevoegd omdat ik de kaart op de grafiek wou hebben met zo min mogelijk marge.
+                )
+
                 st.plotly_chart(fig2, use_container_width=True)
 
         
             except Exception as e:
-                st.image('Pythoncode/amsterdam-map.jpg')
-                st.warning("Kaart ko niet geladen worden, fallback wordt gebruikt.")
+                st.image('amsterdam-map.jpg')
+                st.warning("Kaart kon niet geladen worden, fallback wordt gebruikt.")
                 st.error(str(e))
     
     #_____________________________________________________________________________________
