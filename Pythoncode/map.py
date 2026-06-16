535bc7a1bc30a1e8e19fa8fd0efefbfc53c61fa8
martinsalicia23-spec <martinsalicia23@gmail.com>
2026-06-16 14:55:39 +0200

    Update map.py

--- Pythoncode/map.py
+++ Pythoncode/map.py
@@ -525,7 +525,7 @@ def main_app():
                     #geojson=buurten.__geo_interface__,
                     #locations=buurten.index,
                     geojson=buurten.set_index("kaart_id").__geo_interface__,
-                    locations="kaart_id",
+                    #locations="kaart_id",
                     #featureidkey="id", 
                     color=index,
                     hover_name ='Gebied',
